bf30c9f847709f3a3220a15e7f255c28fb6c434f
Don <donadley@gmail.com>
2015-02-09 12:16:40 -0600

    almost passing object to parse

--- android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/authenticator/RegisterActivity.java
+++ android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/authenticator/RegisterActivity.java
@@ -1,19 +1,72 @@
 package com.lsus.teamcoach.teamcoachapp.authenticator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lsus.teamcoach.teamcoachapp.R;
+import com.lsus.teamcoach.teamcoachapp.R.id;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import butterknife.InjectView;
 
 public class  RegisterActivity extends ActionBarActivity {
 
+
+
+    @InjectView(id.btnRegister) protected Button confirmRegisterButton;
+    @InjectView(id.tvRegisterCancel) protected TextView cancelRegister;
+    @InjectView(id.et_email) protected EditText email;
+    @InjectView(id.et_password) protected EditText password;
+    @InjectView(id.etFirstName) protected EditText firstName;
+    @InjectView(id.etLastName) protected EditText lastName;
+    @InjectView(id.registerRadioGroup) protected RadioGroup radioButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        confirmRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                lastName.getText();
+                email.getText();
+                password.getText();
+                int selectedId = radioButtons.getCheckedRadioButtonId();
+
+                ParseObject parseObject = new ParseObject("User");
+
+                ParseUser user = new ParseUser();
+                user.put("fisrtName", firstName.getText());
+                user.put("lastName", lastName.getText());
+                user.put("username", email.getText());
+                user.put("email",email.getText());
+
+                switch(selectedId){
+                    case 0:
+                        user.put("role", "Coach");
+                        break;
+                    case 1:
+                        user.put("role", "Player");
+                        break;
+                    default:
+                        // handle no selection
+                        break;
+                }
+
+            }
+        });
     }
 
 
@@ -37,5 +90,18 @@ public class  RegisterActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onRegister()
+    {
+        confirmRegisterButton.setOnClickListener(new View.OnClickListener()
+        {
+                public void onClick(View v)
+                {
+
+                }
+
+        });
+        return true;
     }
 }

8144886eef43c9fb5d866a6a940b91bb758c9960
lancequini <lancequini@gmail.com>
2015-03-02 15:32:26 -0600

    Updated Profile Edit Page (Needs Parse REST API)

--- android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/UserActivity.java
+++ android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/UserActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,11 +25,14 @@ public class UserActivity extends BootstrapActivity implements View.OnClickListe
 
     @InjectView(R.id.iv_avatar) protected ImageView avatar;
     @InjectView(R.id.tv_name) protected TextView name;
-    @InjectView(R.id.tv_role) protected TextView role;
     @InjectView(R.id.tv_userEmail) protected TextView email;
     @InjectView(R.id.tv_username) protected TextView username;
+    @InjectView(R.id.tv_role) protected TextView role;
     @InjectView(R.id.button_Edit) protected Button button_Edit;
     @InjectView(R.id.button_Submit) protected Button button_Submit;
+    @InjectView(R.id.et_name) protected EditText et_name;
+    @InjectView(R.id.et_userEmail) protected  EditText et_email;
+    @InjectView(R.id.et_username) protected EditText et_username;
 
     private User user;
 
@@ -62,13 +66,6 @@ public class UserActivity extends BootstrapActivity implements View.OnClickListe
         Account[] accounts = accountManager.getAccountsByType(Constants.Auth.BOOTSTRAP_ACCOUNT_TYPE);
         Account myAccount =  accounts[0];
 
-        if(myAccount.name.equalsIgnoreCase(user.getEmail()))
-        {
-            System.out.println("In Email");
-        }else{
-            System.out.println("NOpe");
-        }
-
         if(myAccount.name.equalsIgnoreCase(user.getUsername())){
             button_Edit.setVisibility(View.VISIBLE);
         }
@@ -91,13 +88,43 @@ public class UserActivity extends BootstrapActivity implements View.OnClickListe
     public void onEdit(){
         button_Edit.setVisibility(View.GONE);
         button_Submit.setVisibility(View.VISIBLE);
+
+        //Resets all TextViews to be EditText fields
+        name.setVisibility(View.GONE);
+        et_name.setVisibility(View.VISIBLE);
+        email.setVisibility(View.GONE);
+        et_email.setVisibility(View.VISIBLE);
+        username.setVisibility(View.GONE);
+        et_username.setVisibility(View.VISIBLE);
+        role.setVisibility(View.GONE);
+
+        //Sets the text in the EditText fields
+        et_name.setText(String.format("%s", name.getText().toString()));
+        et_email.setText(String.format("%s", email.getText().toString()));
+        et_username.setText(String.format("%s", username.getText().toString()));
+
     }
 
     public void onSubmit(){
         button_Submit.setVisibility(View.GONE);
         button_Edit.setVisibility(View.VISIBLE);
+
+        //Resets all EditText fields to be TextViews
+        name.setVisibility(View.VISIBLE);
+        et_name.setVisibility(View.GONE);
+        email.setVisibility(View.VISIBLE);
+        et_email.setVisibility(View.GONE);
+        username.setVisibility(View.VISIBLE);
+        et_username.setVisibility(View.GONE);
+        role.setVisibility(View.VISIBLE);
+
+
+        //Sets the text in the TextViews.
+        //Needs to be changed to update Parse using Rest API!  ------------------------------
+        //Need to introduce checks here!!!! -------------------------------------------------
+        name.setText(String.format("%s", et_name.getText().toString()));
+        email.setText(String.format("%s", et_email.getText().toString()));
+        username.setText(String.format("%s", et_username.getText().toString()));
     }
-
-
 }
 

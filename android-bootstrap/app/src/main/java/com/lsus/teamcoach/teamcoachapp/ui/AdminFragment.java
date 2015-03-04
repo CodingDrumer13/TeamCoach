bc3684ca6ebf5a029fb9cda647c214544b5074e1
lancequini <lancequini@gmail.com>
2015-03-04 10:37:30 -0600

    Added Administration page to the Carousel.

--- android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/AdminFragment.java
+++ android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/AdminFragment.java
@@ -2,13 +2,26 @@ package com.lsus.teamcoach.teamcoachapp.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lsus.teamcoach.teamcoachapp.R;
 
 /**
  * Created by TeamCoach on 3/3/2015.
  */
 public class AdminFragment extends Fragment {
 
-    public void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.admin, container, false);
+
+        // Inflate the layout for this fragment
+        return view;
     }
 }

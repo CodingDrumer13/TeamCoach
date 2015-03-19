11b062dd9af8e047a8f6e3811d1b2fbeac8b7eb7
lancequini <lancequini@gmail.com>
2015-03-18 20:31:34 -0500

    Added admin and owner privileges for drills.

--- android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/DrillInfoActivity.java
+++ android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/DrillInfoActivity.java
@@ -1,27 +1,22 @@
 package com.lsus.teamcoach.teamcoachapp.ui;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.github.kevinsawicki.wishlist.Toaster;
 import com.lsus.teamcoach.teamcoachapp.BootstrapServiceProvider;
-import com.lsus.teamcoach.teamcoachapp.Injector;
 import com.lsus.teamcoach.teamcoachapp.R;
 import com.lsus.teamcoach.teamcoachapp.authenticator.LogoutService;
 import com.lsus.teamcoach.teamcoachapp.core.Drill;
 import com.lsus.teamcoach.teamcoachapp.core.Singleton;
-import com.lsus.teamcoach.teamcoachapp.core.User;
 
 import javax.inject.Inject;
 
 import butterknife.InjectView;
 
 import static com.lsus.teamcoach.teamcoachapp.core.Constants.Extra.DRILL;
-import static com.lsus.teamcoach.teamcoachapp.core.Constants.Extra.USER;
 
 /**
  * Created by TeamCoach on 3/18/2015.
@@ -31,10 +26,15 @@ public class DrillInfoActivity extends BootstrapActivity{
     @Inject protected LogoutService logoutService;
 
     @InjectView(R.id.tv_drill_name) protected TextView drillName;
+    @InjectView(R.id.et_drill_name) protected EditText editName;
     @InjectView(R.id.tv_drill_description) protected TextView drillDescription;
+    @InjectView(R.id.et_drill_description) protected EditText editDescription;
     @InjectView(R.id.tv_drill_rating) protected TextView drillRating;
     @InjectView(R.id.button_drill_edit) protected Button btnEdit;
     @InjectView(R.id.button_drill_submit) protected Button btnSubmit;
+    @InjectView(R.id.tv_drill_times_used) protected TextView timesUsed;
+    @InjectView(R.id.tv_drill_times_used_num) protected TextView timesUsedNum;
+
 
     private Drill drill;
 
@@ -58,7 +58,53 @@ public class DrillInfoActivity extends BootstrapActivity{
             btnEdit.setVisibility(View.VISIBLE);
         }
 
+        if(singleton.getCurrentUser().getRole().equalsIgnoreCase("Admin")){
+            timesUsed.setVisibility(View.VISIBLE);
+            timesUsedNum.setText(String.format("%s", drill.getTimesUsed()));
+            timesUsedNum.setVisibility(View.VISIBLE);
+            btnEdit.setVisibility(View.VISIBLE);
+        }
+
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClick(View view) {
+        if(view.getId() == btnEdit.getId())
+        {
+            //The Edit button has been clicked
+            onEdit();
+        }else if(view.getId() == btnSubmit.getId()){
+            //The Submit button has been clicked
+            onSubmit();
+        }
+    }
+
+    private void onEdit() {
+        drillName.setVisibility(View.GONE);
+        drillDescription.setVisibility(View.GONE);
+
+        editName.setVisibility(View.VISIBLE);
+        editDescription.setVisibility(View.VISIBLE);
+
+        editName.setText(drillName.getText());
+        editDescription.setText(drillDescription.getText());
+
+        btnEdit.setVisibility(View.GONE);
+        btnSubmit.setVisibility(View.VISIBLE);
+    }
+
+    private void onSubmit() {
+        drillName.setVisibility(View.VISIBLE);
+        drillDescription.setVisibility(View.VISIBLE);
+
+        editName.setVisibility(View.GONE);
+        editDescription.setVisibility(View.GONE);
+
+        drillName.setText(editName.getText());
+        drillDescription.setText(editDescription.getText());
+
+        btnSubmit.setVisibility(View.GONE);
+        btnEdit.setVisibility(View.VISIBLE);
     }
 }

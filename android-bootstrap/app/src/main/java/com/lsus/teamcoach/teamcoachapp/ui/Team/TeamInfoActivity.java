6214e1b1a7a8a583d109cdc5fa2fb62067fad34b
Don <donadley@gmail.com>
2015-04-14 00:49:55 -0500

    Refractoried frag to activity (Team_Info_Frag

--- android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/Team/TeamInfoActivity.java
+++ android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/Team/TeamInfoActivity.java
@@ -1,22 +1,14 @@
 package com.lsus.teamcoach.teamcoachapp.ui.Team;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.lsus.teamcoach.teamcoachapp.Injector;
 import com.lsus.teamcoach.teamcoachapp.R;
 import com.lsus.teamcoach.teamcoachapp.authenticator.LogoutService;
 import com.lsus.teamcoach.teamcoachapp.core.BootstrapService;
-import com.lsus.teamcoach.teamcoachapp.core.Constants;
 import com.lsus.teamcoach.teamcoachapp.core.Singleton;
 import com.lsus.teamcoach.teamcoachapp.core.Team;
 import com.lsus.teamcoach.teamcoachapp.ui.Framework.BootstrapActivity;
@@ -60,7 +52,7 @@ public class TeamInfoActivity extends BootstrapActivity implements View.OnClickL
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.team_info_fragment);
+        setContentView(R.layout.team_info_activity);
 
         btnTeamEdit.setOnClickListener(this);
         btnTeamEdit.setOnClickListener(this);

f14f380bd3eec426e70baa32625677f84c4bc90e
CodingDrumer13 <donadley@gmail.com>
2015-04-28 15:04:12 -0500

    Added Color to add news spinner

--- android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/News/AddNewsFragment.java
+++ android-bootstrap/app/src/main/java/com/lsus/teamcoach/teamcoachapp/ui/News/AddNewsFragment.java
@@ -2,6 +2,7 @@ package com.lsus.teamcoach.teamcoachapp.ui.News;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,6 +41,7 @@ public class AddNewsFragment extends Fragment implements View.OnClickListener{
     private SafeAsyncTask<Boolean> authenticationTask;
     private News news;
     protected NewsListFragment newsListFragment;
+    private NewsFragment parent;
 
     @Inject protected BootstrapService bootstrapService;
 
@@ -47,7 +49,8 @@ public class AddNewsFragment extends Fragment implements View.OnClickListener{
     @InjectView(R.id.et_add_news_message) EditText et_add_news_message;
     @InjectView(R.id.btn_send_message) Button btn_send_message;
     @InjectView(R.id.sp_news_team) Spinner sp_news_team;
-    private NewsFragment parent;
+    @InjectView(R.id.btn_new_back) Button btnNewBack;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -62,46 +65,59 @@ public class AddNewsFragment extends Fragment implements View.OnClickListener{
         Views.inject(this, view);
 
         btn_send_message.setOnClickListener(this);
+        btnNewBack.setOnClickListener(this);
 
-        ArrayAdapter<Team> spinnerArrayAdapter = new ArrayAdapter<Team>(this.getActivity(), android.R.layout.simple_spinner_item, singleton.getUserTeams()); //selected item will look like a spinner set from XML
+        ArrayAdapter<Team> spinnerArrayAdapter = new ArrayAdapter<Team>(this.getActivity(), R.layout.teamcoach_spinner_item, singleton.getUserTeams()); //selected item will look like a spinner set from XML
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp_news_team.setAdapter(spinnerArrayAdapter);
     }
 
     @Override
     public void onClick(View v) {
-        showProgress();
-        news = new News();
-        news.setContent(et_add_news_message.getText().toString());
-        news.setCreator(singleton.getCurrentUser().getObjectId());
-        news.setTitle(et_add_news_title.getText().toString());
-        Team team = (Team)sp_news_team.getSelectedItem();
-        news.setTeamId(team.getObjectId());
-//        Current time an date
-        String currentDateandTime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a").format(new Date());
-        news.setTimestamp(currentDateandTime);
 
-        authenticationTask = new SafeAsyncTask<Boolean>() {
-            @Override
-            public Boolean call() throws Exception {
-                bootstrapService.addNews(news);
-                return true;
-            }
-            @Override
-            public void onSuccess(final Boolean authSuccess) {
-                getFragmentManager().popBackStack();
-                newsListFragment.refresh();
-                parent.showButton();
-            }
+        if (v.getId() == btn_send_message.getId()) {
+            //Show Loading Menu
+            showProgress();
 
-            @Override
-            protected void onFinally() throws RuntimeException {
-                hideProgress();
-                authenticationTask = null;
-            }
-        };
-        authenticationTask.execute();
-    }
+            news = new News();
+            news.setContent(et_add_news_message.getText().toString());
+            news.setCreator(singleton.getCurrentUser().getObjectId());
+            news.setTitle(et_add_news_title.getText().toString());
+            Team team = (Team) sp_news_team.getSelectedItem();
+            news.setTeamId(team.getObjectId());
+
+            //Current time an date
+            String currentDateandTime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a").format(new Date());
+            news.setTimestamp(currentDateandTime);
+
+            authenticationTask = new SafeAsyncTask<Boolean>() {
+                @Override
+                public Boolean call() throws Exception {
+                    bootstrapService.addNews(news);
+                    return true;
+                }
+
+                @Override
+                public void onSuccess(final Boolean authSuccess) {
+                    getFragmentManager().popBackStack();
+                    newsListFragment.refresh();
+                    parent.showButton();
+                }
+
+                @Override
+                protected void onFinally() throws RuntimeException {
+                    hideProgress();
+                    authenticationTask = null;
+                }
+            };
+            authenticationTask.execute();
+        }
+        if(v.getId() == btnNewBack.getId()){
+            this.getFragmentManager().popBackStack();
+            parent.showButton();
+        }
+}
+
 
     @SuppressWarnings("deprecation")
     protected void hideProgress() {
